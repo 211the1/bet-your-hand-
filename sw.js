a7a505677ce23e3bf121d93dacbbca660dfe4959
211the1 <211the1@gmail.com>
2026-09-19 19:18:35 -0700

    Fix PWA installability with fetch handler

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
-self.addEventListener('install',event=>self.skipWaiting());
-self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',event=>{});
+self.addEventListener('install', event => self.skipWaiting());
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
