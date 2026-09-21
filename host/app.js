a53fc59dc072a5b71b526a82fe4553e8871b5df7
211the1 <211the1@gmail.com>
2026-09-21 09:40:49 -0700

    Fix Host action effect script line breaks

--- host/app.js
+++ host/app.js
@@ -355,7 +355,8 @@ function connectAndCreate(){
     let message;
     try{message=JSON.parse(event.data)}catch{return}
 
-    if(message.type==='CALL_PLAYER'){showHostActionEffect('CALL_PLAYER');return}\n    if(message.type==='PLAY_YOUR_HAND_EVENT'){showHostActionEffect('PLAY_YOUR_HAND');return}\n    if(message.type==='PLAYER_EMOJI'){showPlayerEmoji(message);return}
+    if(message.type==='CALL_PLAYER'){showHostActionEffect('CALL_PLAYER');return}
+    if(message.type==='PLAY_YOUR_HAND_EVENT'){showHostActionEffect('PLAY_YOUR_HAND');return}\n    if(message.type==='PLAYER_EMOJI'){showPlayerEmoji(message);return}
 
     if(message.type==='ERROR'){
       creating=false;
