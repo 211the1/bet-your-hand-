c5c05869375728ce81298d61cc7b7ac3ea9dd3aa
211the1 <211the1@gmail.com>
2026-09-22 19:59:01 -0700

    Show existing player photos names and scores on host finish

--- host/app.js
+++ host/app.js
@@ -295,7 +295,7 @@ function renderHostFinishScreen(game){
     document.body.appendChild(overlay);
   }
   const scores=[...(Array.isArray(game.players)?game.players:[])].sort((a,b)=>Number(b.points||0)-Number(a.points||0));
-  overlay.innerHTML='<div class="finish-score-overlay">'+scores.slice(0,5).map((p,i)=>'<div class="finish-score-box score-box-'+i+'">'+Number(p.points||0)+'</div>').join('')+'</div>';
+  overlay.innerHTML='<div class="finish-player-overlay">'+scores.slice(0,5).map((p,i)=>{const img=({'Bug':'/Bug.jpg','Face':'/Face.jpg','Ling Ling':'/Ling_Ling.jpg','Beanz':'/Beanz.jpg','The One':'/The_One.jpg','Boone':'/Boone.jpg','Chicken Joe':'/Chicken_Joe.jpg','Juby':'/Juby.jpg','Meemaw':'/Meemaw.jpg'}[p.character]||'');return '<div class="finish-player finish-player-'+i+'">'+(img?'<img class="finish-player-photo" src="'+img+'" alt="">':'')+'<div class="finish-player-name">'+String(p.name||'').replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;')+'</div><div class="finish-player-score">'+Number(p.points||0)+'</div></div>';}).join('')+'</div>';
   document.body.classList.add('host-finished');
 }
 
