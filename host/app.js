17f0698e097c47e2af7fb00f53cb198d950c3c1a
211the1 <211the1@gmail.com>
2026-09-24 20:06:20 -0700

    Fix Host script loader for finish-screen testing

--- host/app.js
+++ host/app.js
@@ -1,10 +1,10 @@
 (() => {
 'use strict';
 
-const core=document.createElement('wscript');
+const core=document.createElement('script');
 core.src='/host/app-core.js?v=1';
 core.onload=()=>{
-  const finish=document.createElement('wscript');
+  const finish=document.createElement('script');
   finish.src='/host/finish-test.js?v=2';
   document.body.appendChild(finish);
 };
